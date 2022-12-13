4ec2594cc34effeb5e713c2cbf41fbec968a603b
Kendall Powell <kendalpowel@hotmail.com>
2022-12-13 15:55:41 -0700

    yeah

--- src/pages/AccountPage.vue
+++ src/pages/AccountPage.vue
@@ -1,9 +1,4 @@
 <template>
-  <!-- <div class="about text-center">
-    <h1>Welcome {{ account.name }}</h1>
-    <img class="rounded" :src="account.picture" alt="" />
-    <p>{{ account.email }}</p>
-  </div> -->
 
   <div class="container-fluid">
     <div class="row justify-content-center">
